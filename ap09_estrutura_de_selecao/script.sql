6046c0f3a1e5e444687e2865c801825c2f7e24d9
Beatris Franklin <beatris.franklin@gmail.com>
2026-03-17 09:21:54 -0300

    learn(ap09); encerra a apostila

--- ap09_estrutura_de_selecao/script.sql
+++ ap09_estrutura_de_selecao/script.sql
@@ -1,5 +1,58 @@
 DO $$
 DECLARE
+ --testar
+ --22/10/2022: valida
+ --29/02/2020: 2020 é bissexto, válida
+ --29/02/2021: inválida
+ --28/02/2021: válida
+ --31/06/2021: inválida
+ data INT := 31062021;
+ dia INT;
+ mes INT;
+ ano INT;
+ data_valida BOOL := TRUE;
+BEGIN
+ dia := data / 1000000;
+ mes := data % 1000000 / 10000;
+ ano := data % 10000;
+ RAISE NOTICE 'A data é %/%/%', dia, mes, ano;
+ RAISE NOTICE 'Validando se a data é válida...';
+IF ano >= 1 THEN
+ CASE
+  WHEN mes > 12 OR mes < 1 OR dia < 1 OR dia > 31 THEN
+   data_valida := FALSE;
+ ELSE
+ IF ((mes = 4 OR mes = 6 OR mes = 9 OR mes = 11) AND dia > 30) THEN
+  data_valida := FALSE;
+ ELSE
+ IF mes = 2 THEN
+  CASE
+   WHEN ((ano % 4 = 0 AND ano % 100 <> 0) OR ANO % 400 = 0) THEN
+    IF dia > 29 THEN
+     data_valida := FALSE;
+    END IF;
+   ELSE
+   IF dia > 28 THEN
+    data_valida := FALSE;
+   END IF;
+  END CASE;
+ END IF;
+END IF;
+END CASE;
+ELSE
+ data_valida := FALSE;
+END IF;
+CASE
+ WHEN data_valida THEN
+  RAISE NOTICE 'A data % é válida', data;
+ ELSE
+  RAISE NOTICE 'A data % é inválida', data;
+END CASE;
+END;
+$$
+
+DO $$
+DECLARE
  valor INT := fn_valor_aleatorio_entre(1, 12);
  mensagem VARCHAR(200);
 BEGIN
